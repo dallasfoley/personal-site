8c684cbb11b0f207a2d58b91be19ee4af54523a6
dallasfoley <dallastfoley@gmail.com>
2024-04-15 15:45:44 -0400

    use client

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import InstagramIcon from "@mui/icons-material/Instagram";
